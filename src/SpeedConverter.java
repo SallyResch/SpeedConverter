c972c050b1375f444fabfdf8b16e99f96ed560b3
Sally Resch <sallyresch27@gmail.com>
2022-08-28 18:12:52 +0200

    done first part of coding exercieses lesson

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -2,11 +2,12 @@ public class SpeedConverter {
 
     public static long toMilesPerHour(double kilometersPerHour){
         if(kilometersPerHour < 0){
+            return -1;
         }
-        return -1;
+        return Math.round(kilometersPerHour/ 1.609);
     }
-
+    //Next lesson starts down here
     public static void printConversion(double kilometersPerHour){
-        
+
     }
 }
