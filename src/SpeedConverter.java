d6ac42c1aff8939e6b98bd9fcd67ee1096cab35a
Sally Resch <sallyresch27@gmail.com>
2022-08-28 18:07:34 +0200

    Initial commit

---
+++ src/SpeedConverter.java
@@ -0,0 +1,12 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+        }
+        return -1;
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        
+    }
+}
