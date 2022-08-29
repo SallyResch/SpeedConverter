806f1162f7fe65fb211b734b5b47158b4c7c4511
Sally Resch <sallyresch27@gmail.com>
2022-08-29 14:12:58 +0200

    added code from 2 part of coding exercise

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,13 +1,22 @@
 public class SpeedConverter {
 
     public static long toMilesPerHour(double kilometersPerHour){
+        //returns a long value
         if(kilometersPerHour < 0){
             return -1;
         }
         return Math.round(kilometersPerHour/ 1.609);
     }
     //Next lesson starts down here
+    //Void = does not have to return anything
     public static void printConversion(double kilometersPerHour){
-
+        if(kilometersPerHour <0){
+            System.out.println("Invalid value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour +
+                    "km/h= " + milesPerHour +
+                    "mi/h ");
+        }
     }
 }
