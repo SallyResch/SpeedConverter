665498b5c323e3364bc56c26de45bf78e3adf9e4
Sally Resch <sallyresch27@gmail.com>
2022-08-30 11:27:46 +0200

    Checked solution and it was correct, yeay

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long miles = SpeedConverter.toMilesPerHour(10.5);
+        long miles = SpeedConverter.toMilesPerHour(2);
         System.out.println("Miles = " + miles);
 
-        SpeedConverter.printConversion(10.5);
+        SpeedConverter.printConversion(-1);
     }
 }
